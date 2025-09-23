4ed892cfc56b8141e62599306ad1c03258454461
xinyaoliu2027-Zoe <xinyaoliu2027@u.northwestern.edu>
2025-09-22 21:25:03 -0500

    Initial Vite React TypeScript project setup

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    react({
+      babel: {
+        plugins: ['babel-plugin-react-compiler'],
+      }
+    })],
+  test: {
+    globals: true,
+    environment: 'jsdom'
+  }
+})
